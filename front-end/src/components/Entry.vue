63606642c004e8f90f843910c61f1528438e5c24
dylan <chenzl25@mail2.sysu.edu.cn>
2017-05-04 00:56:49 +0800

    use api to get movies data

--- front-end/src/components/Entry.vue
+++ front-end/src/components/Entry.vue
@@ -1,49 +1,95 @@
 <template>
-  <nav class="navbar navbar-default">
-    <div class="container-fluid">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">
-          <img id="logo-img" src="../assets/logo.png">
-        </a>
-      </div>
+  <div class="wrapper">
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">
+            <img id="logo-img" src="../assets/logo.png">
+          </a>
+        </div>
 
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-        </ul>
-        <form class="navbar-form navbar-left">
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="找电影">
-          </div>
-          <button type="submit" class="btn btn-default">搜索</button>
-        </form>
-        <ul class="nav navbar-nav navbar-right">
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img id="avatar-img" src="../assets/avatar.png"> <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="#">Separated link</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-  </nav>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+          </ul>
+          <form class="navbar-form navbar-left">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="找电影" v-model="searchText">
+            </div>
+            <button type="submit" class="btn btn-default">搜索</button>
+          </form>
+          <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img id="avatar-img" src="../assets/avatar.png"> <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+    <div class="row col-xs-1 col-md-1"></div>
+    <div class="row col-xs-10 col-md-10">
+      <template v-for="movie,index in filteredMovies">
+        <div class="movie-item　col-xs-2 col-md-2">
+          <a :href="index">
+            <img class="movie-img"　:src="movie.img" >
+          </a>
+          <div class="movie-name">{{movie.nm}}</div>
+          <div class="movie-scroe">{{movie.sc?movie.sc:'暂无'}}</div>
+        </div>
+      </template>
+    </div>
+    <div class="row col-xs-1 col-md-1"></div>
+  </div>
 </template>
 
 <script>
+import $ from '../../node_modules/jquery/dist/jquery.min.js'
+
+let entryData = {}
+
+entryData.movies = [1]
+
+$.ajax({url: 'api/movie/list.json?type=hot&offset=0&limit=40'})
+ .done(function (data) {
+   data = JSON.parse(data)
+   entryData.movies = data['data']['movies']
+ })
+
 export default {
-  name: 'entry'
+  name: 'entry',
+  data: (function (entryData) {
+    return function () {
+      return {
+        entryData: entryData,
+        searchText: ''
+      }
+    }
+  })(entryData),
+  computed: {
+    filteredMovies: function () {
+      var self = this
+      if (self.searchText === '') {
+        return self.entryData.movies
+      }
+      return self.entryData.movies.filter(function (movie) {
+        return movie.nm.indexOf(self.searchText) !== -1
+      })
+    }
+  }
 }
 </script>
 
@@ -60,5 +106,27 @@ export default {
   height: 35px;
   padding: 0;
   margin: 0;
+}
+.movie-img {
+  width: 165px;
+  height: 230px;
+  margin: 10px;
+  padding: 10px;
+}
+.movie-item {
+
+}
+.movie-name {
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  width: 165px;
+  font-size: 16px;
+  padding-left: 20px;
+  font-family: Microsoft YaHei,Helvetica,Arial,sans-serif;
+}
+.movie-scroe {
+  font-size: 20px;
+  padding-left: 20px;
+  color: orange;
 }
 </style>
