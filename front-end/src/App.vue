b189b63dff83baec72e84e5136532eb26a74a681
dylan <chenzl25@mail2.sysu.edu.cn>
2017-05-03 17:54:09 +0800

    init front-end

---
+++ front-end/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+
+</style>
